80a39aa16f481e8db846a8feb7a83383c12c3adc
Taras Iagniuk <mrtaryk@gmail.com>
2013-06-01 23:48:15 +0300

    Escape quotes!

--- notify_translate.sh
+++ notify_translate.sh
@@ -2,4 +2,5 @@
 # notify-send -u critical "$(xsel -o)" "$(wget -U "Mozilla/5.0" -qO - "http://translate.google.com/translate_a/t?client=t&text=$(xsel -o | sed "s/[\"'<>]//g")&sl=en&tl=ru" | sed 's/\[\[\[\"//' | cut -d \" -f 1)"
 # notify-send "$(xsel -o)" "$(wget -U "Mozilla/5.0" -qO - "http://translate.google.com/translate_a/t?client=t&text=$(xsel -o | sed "s/[\"'<>]//g")&sl=en&tl=uk" | sed 's/\[\[\[\"//' | cut -d \" -f 1)"
 
-notify-send "$(xsel -o)" "$(python ~/bin/tran.py "$(xsel -o | sed "s/[\"'<>]//g")")"
+SELECTED=`xsel -o`
+notify-send "$SELECTED" "$(python ~/bin/tran.py "$(echo $SELECTED | sed "s/([\"'])/\\\1/g")" 2>&1)"
