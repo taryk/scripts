c285538dabee91f0428267138e58c7eadbd5dc6f
Taras Iagniuk <mrtaryk@gmail.com>
2013-06-01 23:57:51 +0300

    Parse json instead of evaluating incoming data using `eval`; Make strings unicode; Minor fixes

--- tran.py
+++ tran.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*- 
 
 import httplib, urllib, argparse, re
+import json
+# import pprint
+
+SRC_LANG = 'auto'
+TO_LANG  = 'uk'
+HOST     = 'translate.google.com'
 
 def argparser():
     parser = argparse.ArgumentParser(description='Instant Translator')
-    parser.add_argument('phrase', metavar='p', type=str,
+    parser.add_argument('phrase', metavar='p', type=unicode,
                         help='phrase to translate')
     args = parser.parse_args()
     return args.phrase
@@ -15,32 +21,37 @@ def retrieve(host, path):
     request.request("GET", path, "", {'User-Agent' : 'Mozilla/5.0'})
     response = request.getresponse()
     if response.status > 200:
-        return '[["{}"],"ERROR"]'.format(response.reason)
+        return u'[["{}"],"ERROR"]'.format(response.reason)
     return response.read()
 
 def parse_item(item, depth = 0):
-    if type(item) is str:
+    if type(item) is list:
+        result = u''
+        for sub_item in item:
+            result += parse_item(sub_item, depth + 1)
+        return result
+    elif type(item) is unicode:
         if len(item) == 0:
-            return ''
-        return "{}{}\n".format("\t" * depth, item)
-    result = ''
-    for sub_item in item:
-        result += parse_item(sub_item, depth + 1)
-    return result
+            return u''
+        return u"{}{}\n".format(u"\t" * depth, item)
+    else:
+        return u''
 
-phrase = argparser()
-host = 'translate.google.com'
-URL = 'translate_a/t?client=t&sl=auto&tl=uk&' + urllib.urlencode({'text' : phrase})
-
-response = re.sub(',{2,}', ',', retrieve(host, "/" + URL))
-try:
-    translated = eval(response)
-    result = ''
-    for item in translated:
-        if type(item) is str:
-            result = "Translation: {} > uk\n\n{}".format(item, result)
-            break
-        result += parse_item(item, -1)
-    print result
-except RuntimeError as ex:
-    print "Something went wrong ({}): {}".format(response, ex)
+if __name__ == '__main__':
+    phrase = argparser()
+    path = 'translate_a/t?client=t&sl=' + SRC_LANG + '&tl=' + TO_LANG + '&' + urllib.urlencode({'text' : phrase})
+    response = re.sub(',{2,}', ',', retrieve(HOST, "/" + path))
+    try:
+        # translated = eval(response)
+        translated = json.loads(response)
+        # pp = pprint.PrettyPrinter(indent=2)
+        # pp.pprint(translated)
+        result = ''
+        for item in translated:
+            if type(item) is unicode:
+                result = u"Translation: {} > {}\n\n{}".format(item, TO_LANG, result)
+                # break
+            result += parse_item(item, -1)
+        print result.encode('utf8')
+    except RuntimeError as ex:
+        print u'Something went wrong ({}): {}'.format(response, ex)
